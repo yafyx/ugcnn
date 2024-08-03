db30eff894a8333d88cc92440d6daa22c1a7b32e
Yafik <mhmmdyfik@gmail.com>
2024-08-04 05:26:31 +0700

    first commit

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "UG Timeline",
+  description: "",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  navMenuItems: [
+
+  ],
+  links: {
+
+  },
+};
